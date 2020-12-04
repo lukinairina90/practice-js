1a5e06324d6aad5183f58e14aa68450c5448f742
Irina <irinalukina90@gmail.com>
2020-12-04 12:44:41 +0200

    Add button - "Change color"

--- js/main.js
+++ js/main.js
@@ -1,11 +1,27 @@
-let a = 10
-let b = 1
+let a = 10;
+let b = 1;
+
+let button1 = document.getElementById("super-id");
+let button2 = document.getElementById("changeColor");
 
 function sum(a, b) {
     alert(a+b);
 }
 
-let button = document.getElementById("super-id")
-button.addEventListener("click", function () {
+button1.addEventListener("click", function () {
     sum(a, b);
 });
+
+button2.addEventListener("click", function () {
+        r = Math.floor(Math.random() * (256)),
+        g = Math.floor(Math.random() * (256)),
+        b = Math.floor(Math.random() * (256)),
+        color = '#' + r.toString(16) + g.toString(16) + b.toString(16);
+        // console.log(color);
+
+    button2.style.background = color;
+    // button2.style.color = color;
+    // button2.innerText = color;
+});
+
+
